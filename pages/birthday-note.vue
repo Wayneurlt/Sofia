c9d42fa7b1527ba87dff5d5e1bc0e71f67da7fdb
Wayneurlt <wayneurlt@gmail.com>
2026-03-17 17:42:59 +0300

    Remove after-yes.vue component and add new birthday-themed pages: birthday-note.vue and birthday-story.vue for enhanced user experience. These pages feature a simple layout and are designed to celebrate birthdays.

---
+++ pages/birthday-note.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center p-4">
+    <h1 class="font-display text-2xl text-stone-800">Birthday note</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Birthday note page
+</script>
---
+++ pages/birthday-story.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center p-4">
+    <h1 class="font-display text-2xl text-stone-800">Birthday story</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Birthday story page
+</script>
